147ab2f57a9947261891d8e443f531f160923a1f
nieja-obs <parowka37@outlook.com>
2025-11-13 12:56:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,45 +4,56 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
-
-// Serwujemy pliki statyczne z katalogu "public"
+// Serwowanie plików statycznych z katalogu "public"
 app.use(express.static('public'));
 
-let users = [];  // lista nicków aktualnie zalogowanych
+let users = []; // { socketId, nickname, room }
 
+// Połączenie nowego klienta
 io.on('connection', (socket) => {
-  let userNick = '';
+  let currentUser = null;
 
-  // Użytkownik dołącza do czatu
-  socket.on('join', (nick) => {
-    userNick = nick;
-    users.push(nick);
-    io.emit('user list', users);
-    console.log(`${nick} dołączył do czatu`);
+  // Dołączenie do czatu
+  socket.on('join', ({ nickname, room }) => {
+    currentUser = { socketId: socket.id, nickname, room };
+    users.push(currentUser);
+
+    socket.join(room);
+
+    // Aktualizacja listy użytkowników tylko w tej samej sali
+    const roomUsers = users.filter(u => u.room === room).map(u => u.nickname);
+    io.to(room).emit('user list', roomUsers);
+
+    // Powiadomienie o dołączeniu
+    io.to(room).emit('user joined', nickname);
+    console.log(`${nickname} dołączył do czatu w pokoju ${room}`);
   });
 
-  // Odbieramy i rozsyłamy wiadomość
+  // Wiadomości
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    console.log(`Wiadomość od ${msg.user}: ${msg.text}`);
+    io.to(msg.room).emit('chat message', msg);
+    console.log(`Wiadomość od ${msg.user} w ${msg.room}: ${msg.text}`);
   });
 
-  // Użytkownik zaczął pisać
+  // Typing indicator
   socket.on('typing', (nick) => {
-    socket.broadcast.emit('typing', nick);
+    if (!currentUser) return;
+    socket.to(currentUser.room).emit('typing', nick);
   });
 
-  // Użytkownik przestał pisać
   socket.on('stop typing', (nick) => {
-    socket.broadcast.emit('stop typing', nick);
+    if (!currentUser) return;
+    socket.to(currentUser.room).emit('stop typing');
   });
 
-  // Użytkownik rozłącza się
+  // Rozłączenie
   socket.on('disconnect', () => {
-    if (userNick) {
-      users = users.filter(u => u !== userNick);
-      io.emit('user list', users);
-      console.log(`${userNick} opuścił czat`);
+    if (currentUser) {
+      users = users.filter(u => u.socketId !== socket.id);
+      const roomUsers = users.filter(u => u.room === currentUser.room).map(u => u.nickname);
+      io.to(currentUser.room).emit('user list', roomUsers);
+      io.to(currentUser.room).emit('user left', currentUser.nickname);
+      console.log(`${currentUser.nickname} opuścił czat`);
     }
   });
 });
